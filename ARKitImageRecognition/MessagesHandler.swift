ae9a049ea6510fa895f09fa5934390d93260f1ff
Eran Heres <eran.heres@gmail.com>
2018-04-26 19:21:02 +0300

    .

---
+++ ARKitImageRecognition/MessagesHandler.swift
@@ -0,0 +1,27 @@
+//
+//  MessagesHandler.swift
+//  ARKitImageRecognition
+//
+//  Created by Eran Heres on 25/04/2018.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import Foundation
+
+class NetworkApplicationLayer {
+    func message(str : String) {
+        let dict = convertToDictionary(text: str)
+    }
+
+    func convertToDictionary(text: String) -> [String: Any]? {
+        if let data = text.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
+
+}
